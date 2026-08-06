d3c93e9cca0b54f166802894c08801c688b9d330
sprakhar639 <sprakhar639@gmail.com>
2026-08-06 22:45:45 +0530

    completed the CRUD ops:- Create, Get_All, Get_By_ID, Update, Delete

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -6,13 +6,51 @@ async function createBlog(req,res){
 
    const blog=await blogModel.create({title,content})
 
-   console.log(title);
-   console.log(content);
-
    res.status(201).json({
     message:"Blog Created Successfully",blog
    })
 }
 
+async function getAllBlogs(req,res){
+    const blogs=await blogModel.find()
+    res.status(201).json({
+        message:"All blogs fetched successfully",
+        blogs
+    })
+}
 
-module.exports={createBlog}
+async function getBlogById(req,res){
+    const {id}=req.params;
+    const blog=await blogModel.findById(id)
+    res.status(201).json({
+        message:"Blog by id fetched successfully",
+        blog
+    })
+}
+
+async function updateBlogById(req,res){
+    const {title,content}=req.body
+    const {id}=req.params
+    const blog=await blogModel.findByIdAndUpdate(id,{content},{new:true})
+    res.status(201).json({
+        message:"Blog Updated Successfully",
+        blog
+    })
+}
+
+async function deleteBlogById(req,res){
+    const {id}=req.params
+    const blog=await blogModel.findByIdAndDelete(id)
+     if (!blog) {
+        return res.status(404).json({
+            message: "Blog not found"
+        });
+    }
+    res.status(200).json({
+        message:"Blog deleted Successfully",
+    })
+
+
+}
+
+module.exports={createBlog,getAllBlogs,getBlogById,updateBlogById,deleteBlogById}
--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -6,4 +6,8 @@ const router=express.Router()
 
 
 router.post("/",blogController.createBlog)
+router.get("/",blogController.getAllBlogs)
+router.get("/:id",blogController.getBlogById)
+router.put("/:id",blogController.updateBlogById)
+router.delete("/:id",blogController.deleteBlogById)
 module.exports=router
