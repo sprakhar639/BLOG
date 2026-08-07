2ecda1183a51e7bbec8cbb1e76aa9f2f3bfbdbae
sprakhar639 <sprakhar639@gmail.com>
2026-08-07 20:48:41 +0530

    implemented login and logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -32,12 +32,56 @@ async function registerUser(req,res){
 
 
 res.cookie("token",token)
+const userObj = user.toObject();
+
+delete userObj.password;
 
  res.status(201).json({
-        message:"User registed Successfully",user
+        message:"User registed Successfully",user:userObj
        })
 
     }
 
 
-module.exports={registerUser}
+async function loginUser(req,res){
+    const{username,email,password}=req.body
+    
+    const user=await userModel.findOne({
+        $or:[
+            {username},{email}
+        ]
+    })
+
+    
+
+    if(!user){
+        return res.status(401).json({message:"User not exists"})
+    }
+
+    const isPasswordValid= await bcrypt.compare(password,user.password)
+
+
+    if(!isPasswordValid){
+        return res.status(401).json({message:"Wrong Password"})
+    }
+
+
+        const token=jwt.sign({
+            id:user._id},
+            process.env.JWT_SECRET
+        )
+        res.cookie("token",token)
+
+        const userObj=user.toObject()
+        delete userObj.password
+        res.status(200).json({
+            message:"Logged In Successfully",user:userObj
+        })
+}
+
+
+async function logOutUser(req,res){
+    res.clearCookie("token")
+    res.status(201).json({message:"Logged Out Successfully"})
+}
+module.exports={registerUser,loginUser,logOutUser}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,8 @@ const userController=require('../controllers/user.controller')
 const router=express.Router()
 
 router.post('/register',userController.registerUser)
+router.post('/login',userController.loginUser)
+router.post('/logout',userController.logOutUser)
 
 
 module.exports=router
