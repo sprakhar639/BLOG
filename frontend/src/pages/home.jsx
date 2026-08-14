ef6a0dc780fd79700846b6091a8732fcb9134a71
sprakhar639 <sprakhar639@gmail.com>
2026-08-14 14:31:54 +0530

    Implemented code in homepage to get all blogs

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
+import api from "../api/axios";
+
 function Home() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const getBlogs = async () => {
+      try {
+        const response = await api.get("/blog/");
+        console.log("API RESPONSE:", response.data);
+        setBlogs(response.data.blogs);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getBlogs();
+  }, []);
+
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>You successfully reached Home.</p>
+      <h1>All Blogs</h1>
+
+      {blogs.map((blog) => (
+        <div key={blog._id}>
+          <h2>{blog.title}</h2>
+          <p>{blog.content}</p>
+        </div>
+      ))}
     </div>
   );
 }
